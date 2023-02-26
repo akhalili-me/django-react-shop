d3b199b03a825d2ea1b71277736f0102313050df
Amir Khalili <akhalili.dev@gmail.com>
2023-02-27 00:47:51 +0330

    Create custom user model in accounts app.

---
+++ backend/accounts/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from accounts.managers import UserManager
+from django.utils import timezone
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True,db_index=True)
+    username = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
