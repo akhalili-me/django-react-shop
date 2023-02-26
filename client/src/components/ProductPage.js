2c8336865d24a3ac005c70aaff09ba94b7713dfd
Amir Khalili <akhalili.dev@gmail.com>
2023-02-27 00:13:15 +0330

    Minor optimizatin in ProductPage component.

--- client/src/components/ProductPage.js
+++ client/src/components/ProductPage.js
@@ -8,13 +8,18 @@ import Rating from '../components/Rating'
 
 
 const ProductPage = ({product}) => {
+    const { _id, name, image, countInStock, price, rating, description } = product;
     const images = [
         {
-          'id': product._id,
-          'name': product.name,
-          'src': product.image,
+          'id': _id,
+          'name': name,
+          'src': image,
         },
     ]
+    
+
+    const [status, product_price] = countInStock === 0 ? ['Out of Stock', 'Out of Stock'] : ['In Stock', price];
+
 
   return (
     <Row>
@@ -23,30 +28,35 @@ const ProductPage = ({product}) => {
         </Col>
 
         <Col md={6}>
-            <h2 className='bold line'>{product.name}</h2>
+            <h2 className='bold line'>{name}</h2>
             <Row>
                 <Col md={8} className='info_box_container'>
-                    <div className='line'><Rating value={product.rating}/></div>
-                    <div className='line'><strong>Price: ${product.price}</strong> </div>
-                    <p><strong>Description:</strong> {product.description}</p>
+                    <div className='line'><Rating value={rating}/></div>
+                    <div className='line'><strong>Price: {product_price}</strong> </div>
+                    <p><strong>Description:</strong> {description}</p>
                 </Col>
                     
                 <Col md={4} className='buy_box_container'>
-                        <ListGroup >
-                            <ListGroup.Item>Status: In Stock</ListGroup.Item>
-                            <ListGroup.Item>      
-                                <Form.Group className="" controlId="formBasicEmail">
-                                    <Form.Control min={1} type="number" placeholder="Quantity" />
-                                </Form.Group>
-                            </ListGroup.Item>
-                            <ListGroup.Item><Button variant="primary">Add to Cart</Button></ListGroup.Item>
-                        </ListGroup>
+                <ListGroup className='text-center'>
+                    <ListGroup.Item key="status">{status}</ListGroup.Item>
+                    {countInStock > 0 && (
+                        <>
+                        <ListGroup.Item key="quantity">
+                            <Form.Group controlId="formBasicEmail">
+                            <Form.Control min={1} type="number" placeholder="Quantity" />
+                            </Form.Group>
+                        </ListGroup.Item>
+                        <ListGroup.Item key="add-to-cart">
+                            <Button variant="primary">Add to Cart</Button>
+                        </ListGroup.Item>
+                        </>
+                    )}
+                </ListGroup>    
                 </Col>
             </Row>
         </Col>
-
     </Row>
   )
 }
 
-export default ProductPage
+export default ProductPage;
