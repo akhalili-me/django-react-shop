d6e6ecce90154ddd604994efda8f6f0fc18e5687
Aleksandr Soloshenko <capcom2me@gmail.com>
2023-08-06 21:19:43 +0700

    Fixed: pass kwargs to User constructor

--- backend/modules/accounts/managers.py
+++ backend/modules/accounts/managers.py
@@ -2,11 +2,11 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, username=None):
+    def create_user(self, email, password=None, username=None, **kwargs):
         if not email:
             raise ValueError("The Email field must be set")
         email = self.normalize_email(email)
-        user = self.model(email=email, username=username)
+        user = self.model(email=email, username=username, **kwargs)
         user.set_password(password)
         user.save()
         return user
