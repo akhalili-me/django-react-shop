38d799fa126251c3313f03b80edfe163c0b1101b
Amir Khalili <akhalili.dev@gmail.com>
2023-09-22 19:56:16 +0330

    fixing set up database on run_backend_tests ci.

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -4,11 +4,11 @@ DEBUG = True
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "localhost",
+        "HOST": "127.0.0.1",
         "PORT": "5432",
     }
 }
